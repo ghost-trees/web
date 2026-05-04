0a0883a1ad4ef30cdd25466abd58be0012bd5299
cam <cowens70@gatech.edu>
2026-05-04 09:59:21 -0700

    refactor(map): remove unused selected radius and reduce default point size

--- src/components/map/constants.ts
+++ src/components/map/constants.ts
@@ -8,9 +8,8 @@ export const DEFAULT_STADIA_STYLE_URL =
   'https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json';
 export const POINT_LAYER_ID = 'tree-points-layer';
 export const BRUSH_MIN_PIXELS = 4;
-export const POINT_RADIUS_DEFAULT = 4;
-export const POINT_RADIUS_SELECTED = 6;
-export const POINT_FILL_COLOR_DEFAULT: [number, number, number, number] = [164, 211, 147, 220];
+export const POINT_RADIUS_DEFAULT = 3;
+export const POINT_FILL_COLOR_DEFAULT: [number, number, number, number] = [164, 211, 147, 150];
 export const POINT_FILL_COLOR_SELECTED: [number, number, number, number] = [255, 220, 64, 255];
 export const POINT_LINE_COLOR_DEFAULT: [number, number, number, number] = [104, 155, 90, 255];
 export const POINT_LINE_COLOR_SELECTED: [number, number, number, number] = [255, 244, 153, 255];
