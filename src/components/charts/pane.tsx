9b7a7f41167cd50774472c653c10620e7bafe058
cam <cowens70@gatech.edu>
2026-06-03 18:52:59 -0700

    fix(charts): remove  description from ChartsPane component

--- src/components/charts/pane.tsx
+++ src/components/charts/pane.tsx
@@ -12,12 +12,7 @@ export function ChartsPane() {
     <section aria-label="Charts" className="flex h-full min-h-0 flex-col">
       <PaneHeader
         title="Charts"
-        description={
-          <>
-            {/* TODO: Update this description. */}
-            Chart views.
-          </>
-        }
+        description=""
         onClose={showMapPane}
       />
       <div className="flex-1 min-h-0 overflow-y-auto pb-2 pr-1">
